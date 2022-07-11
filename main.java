a1de9e65a4a4a26a7d24ebeea886fffcde94cbc3
mohitmehta03 <m.mehta03@outlook.com>
2022-07-11 08:39:30 +0530

    updated by mohit

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class main
+{
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		System.out.println("who is This?");
+		System.out.println("hello welcome the program Mr." +name " \n This Program is created by Mohit Mehta(The Mehta)");
+		
+	}
+}
